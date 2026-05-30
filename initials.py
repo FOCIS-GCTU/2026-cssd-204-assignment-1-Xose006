bfa53830d622ee0b0ebc8f505215e26683a2c8c6
Xose006 <2425403516@live.gctu.edu.gh>
2026-05-30 21:38:01 +0000

    Update initials.py
    Assignment 1. Xose Benedicta Afia. Level 200.

--- initials.py
+++ initials.py
@@ -1,7 +1,37 @@
+# File: initials.py
+# Description: Print out my initials
+# Assignment Number: 1
+#
+# Name: Augustus Buckman
+# STUDENT ID: 2425403516
+# Email: 2425403516@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Xose Benedicta Afia, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    #A Calligram of my initials
 
+ print( )
+
+ print("...BAX")
+
+ print( )
+
+ print("...BBBBBBBB............AAAAAAAAAAAA........XXXX....XXXX.....")
+ print("...BBBBBBBBBB..........AAAAAAAAAAAA........XXXX....XXXX.....")
+ print("...BB........BB........AAAA....AAAA.........XXXX..XXXX......")
+ print("...BB........BB........AAAA....AAAA..........XXX..XXX.......")
+ print("...BBBBBBBBBB..........AAAAAAAAAAAA............XXXX.........")
+ print("...BBBBBBBBBB..........AAAAAAAAAAAA............XXXX.........")
+ print("...BB........BB........AAAA....AAAA..........XXX..XXX.......")
+ print("...BB........BB........AAAA....AAAA.........XXXX..XXXX......")
+ print("...BBBBBBBBBB.....**...AAAA....AAAA...**...XXXX....XXXX...**")
+ print("...BBBBBBBB.......**...AAAA....AAAA...**...XXXX....XXXX...**")
+
+ print( )
 
 
 main()
